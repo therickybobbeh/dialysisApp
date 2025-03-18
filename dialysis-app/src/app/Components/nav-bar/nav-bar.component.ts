f50475880fd4e5de480c00fef87fd099419288fe
therickybobbeh <rcole2597@gmail.com>
2025-03-17 21:34:20 -0400

    checkpoint

--- dialysis-app/src/app/Components/nav-bar/nav-bar.component.ts
+++ dialysis-app/src/app/Components/nav-bar/nav-bar.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {ButtonDirective} from "primeng/button";
 import {Toolbar} from "primeng/toolbar";
+import {NgStyle} from "@angular/common";
 
 @Component({
   selector: 'app-nav-bar',
     imports: [
         ButtonDirective,
-        Toolbar
+        Toolbar,
+        NgStyle
     ],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
