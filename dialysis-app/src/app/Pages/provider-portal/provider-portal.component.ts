b097b015f4adb1ec122f648d141c21ba7f4f4f4a
therickybobbeh <rcole2597@gmail.com>
2025-03-17 20:17:09 -0400

    signup page

--- dialysis-app/src/app/Pages/provider-portal/provider-portal.component.ts
+++ dialysis-app/src/app/Pages/provider-portal/provider-portal.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import {TabPanel, TabView} from "primeng/tabview";
+import {NotificationsComponent} from "../../Components/notifications/notifications.component";
 
 @Component({
   selector: 'app-provider-portal',
-  imports: [],
+  imports: [
+    TabView,
+    TabPanel,
+    NotificationsComponent
+  ],
   templateUrl: './provider-portal.component.html',
   styleUrl: './provider-portal.component.scss'
 })
 export class ProviderPortalComponent {
+  activeIndex: number = 0;
 
+  setActiveIndex(index: number) {
+    this.activeIndex = index;
+  }
 }
--- dialysis-app/src/app/Pages/signup/signup.component.ts
+++ dialysis-app/src/app/Pages/signup/signup.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import {Button} from "primeng/button";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+    imports: [
+        Button,
+        FormsModule,
+        NgForOf,
+        ReactiveFormsModule
+    ],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
+    email = '';
+    password = '';
+    loginTypes = [
+        { loginType: 'Patient' },
+        { loginType: 'Provider' }
+    ];
+    selectedLoginType = '';
 
+    onSubmit() {
+        console.log('Logging in with:', this.email, this.password);
+        alert(`Login Attempt: ${this.email}`);
+    }
 }
