107c897ddd0dd891a72babd1cc446f53434a94b9
therickybobbeh <rcole2597@gmail.com>
2025-04-07 13:46:10 -0400

    graphs

--- dialysis-app-ui/src/app/Components/nav-bar/nav-bar.component.ts
+++ dialysis-app-ui/src/app/Components/nav-bar/nav-bar.component.ts
@@ -1,9 +1,10 @@
-import {Component} from '@angular/core';
+import {Component, OnInit, OnDestroy} from '@angular/core';
 import {ButtonDirective, ButtonIcon, ButtonLabel} from "primeng/button";
 import {Toolbar} from "primeng/toolbar";
 import {NgStyle} from "@angular/common";
-import {Router} from "@angular/router";
+import {Router, NavigationEnd, NavigationStart} from "@angular/router";
 import {AuthService} from "../../Services/authentication.service";
+import {Subscription} from 'rxjs';
 
 @Component({
     selector: 'app-nav-bar',
@@ -17,8 +18,30 @@ import {AuthService} from "../../Services/authentication.service";
     templateUrl: './nav-bar.component.html',
     styleUrl: './nav-bar.component.scss'
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit, OnDestroy {
+    protected title = 'Portal';
+    private routerSubscription: Subscription = new Subscription();
+
     constructor(private router: Router, private authService: AuthService) {
+    }
+
+    ngOnInit() {
+        this.routerSubscription.add(this.router.events.subscribe(event => {
+            console.log("event" + event);
+            if (event instanceof NavigationEnd) {
+                if (event.urlAfterRedirects.includes('provider')) {
+                    this.title = 'Provider Portal';
+                } else if (event.urlAfterRedirects.includes('patient')) {
+                    this.title = 'Patient Portal';
+                } else {
+                    this.title = 'Portal';
+                }
+            }
+        }));
+    }
+
+    ngOnDestroy() {
+        this.routerSubscription.unsubscribe();
     }
 
     logout(): void {
